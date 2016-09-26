70f7c32abff3988afc3b16d0f993f6e1d77b6113
tenoglez <teno_glez@hotmail.com>
2016-09-26 14:06:07 +0200

    Package documentation using roxygen2 created

---
+++ geoloc/R/geoloc.R
@@ -0,0 +1,24 @@
+#' A package for connecting to Google Geolocation API
+#'
+#' @description The \code{geoloc} package provides an interface to connect with the \code{Google Geolocation API} with 
+#' functions that convert adresses into coordinates and vice versa.
+#' 
+#' @details The package provides the following functions:
+#' \itemize{
+#'    \item \code{Func1}: 
+#'    \item \code{Func2}: 
+#' }
+
+#'
+#' @author \itemize{
+#'      \item Teno González Dos Santos - \url{https://github.com/tenoglez}
+#'      \item Josué Álvarez Robles - \url{https://github.com/ejarkm}
+#'      \item Jose Luis López Ruiz - \url{https://github.com/pepelu8000}
+#'    }
+#' 
+#' @references Google Geolocation API - \url{https://developers.google.com/maps/documentation/geocoding/intro}\cr
+#'
+#'
+#' @docType package
+#' @name geoloc
+"_PACKAGE"
