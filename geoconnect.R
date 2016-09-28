a6dc2ea6a6bfc6b26aea74e1c2f256b8227e3b26
tenoglez <teno_glez@hotmail.com>
2016-09-28 18:11:46 +0200

    geo.connect function now returns a data.frame

--- geoconnect.R
+++ geoconnect.R
@@ -1,16 +1,18 @@
 library(RCurl)
 library(rjson)
+
+
 geo.connect <- function(address){
   
-  addresses <- list()
+  addresses <- data.frame(latitude = NULL, longitude = NULL, address = NULL)
   for ( i in 1:length(address) ){
     URL <- URLencode(paste("http://maps.google.com/maps/api/geocode/json?address=", address[i], sep=""))
     URL.data <- fromJSON(getURL(URL))
     latitude <- URL.data$results[[1]]$geometry$location$lat
     longitude <- URL.data$results[[1]]$geometry$location$lng
     formatted_address <- URL.data$results[[1]]$formatted_address
-    address.data <- list(latitude = latitude, longitude = longitude, address = formatted_address)
-    addresses[[i]] <- address.data
+    address.data <- data.frame(latitude = latitude, longitude = longitude, address = formatted_address)
+    addresses <- rbind(addresses, address.data)
   }
   return(addresses)
   
