11cb3641940dce65570853e902c9dcbf8befc3e5
ejarkm <ejarkm@gmail.com>
2016-09-29 00:28:05 +0200

    Corrected some problems with coordinates on geo.connect.csv

--- geoconnectCSV.R
+++ geoconnectCSV.R
@@ -16,15 +16,19 @@ geo.connect.csv <- function(address.csv){
         coord.position <- which(search.address == "")
         no.na <- which(complete.cases(address.csv[coord.position, c(lati.position, long.position)]))
         coord.position <- coord.position[no.na]
-          search.address[coord.position] <- 
-            paste(address.csv[, lati.position][coord.position], address.csv[, long.position][coord.position], sep = ", ")
+        search.address[coord.position] <- 
+          paste(address.csv[, lati.position][coord.position], address.csv[, long.position][coord.position], sep = ", ")
       }
-    }else{
-      if ((lati.column) & (long.column)){
-        lati.position <- which(tolower(names(address.csv)) == "latitude")
-        long.position <- which(tolower(names(address.csv)) == "longitude")
-        search.address <- paste(address.csv[, lati.position], address.csv[, long.position], sep = ", ")
-      }
+    }
+  }else{
+    if ((lati.column) & (long.column)){
+      lati.position <- which(tolower(names(address.csv)) == "latitude")
+      long.position <- which(tolower(names(address.csv)) == "longitude")
+      coord.position <- which(complete.cases(address.csv[, c(lati.position, long.position)]))
+      search.address[coord.position] <- 
+        paste(address.csv[, lati.position][coord.position], address.csv[, long.position][coord.position], sep = ", ")
+      coord.position.na <- which(!complete.cases(address.csv[, c(lati.position, long.position)]))
+      search.address[coord.position.na] <- ""
     }
   }
   geo.connect.vector(search.address)
