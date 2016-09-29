bebdb6a9cbcb31f497d37f544272a09da6114059
pepelu8000 <joselu_1993@hotmail.es>
2016-09-29 16:52:57 +0200

    testing finished

--- test.R
+++ test.R
@@ -1,97 +1,84 @@
 # testing 
 
 library(testthat)
-#library(geoloc) ???!!!
-
-
-# creating some examples for testing
-ad <- c("Madrid", "Rydsvagen 254, Linkoping")
-coo.mad <- c(40.41678, -3.70379)
-coo.lin <- c(58.41236, 15.56107 )
-add <- c("Madrid, Spain", "Rydsvägen 254, 584 31 Linköping, Sweden")
-
-# testing the limits of the API configuration  ???!!!
-# URL.data$status == "OK"
+library(geoloc) 
 
 # testing the inputs and outputs of our functions
-# como controlo address del geo.connect  ??!!
-expect_true(is.data.frame(class(geo.connect("Madrid"))))
-expect_true(is.data.frame(class(geo.connect.vector(c("Madrid", "Linkoping")))))
-expect_true(is.data.frame(class(geo.connect.csv(""))))
-
-# pruebas:
-# 1) address
-# 2) coordenada (ll)
-# 3) vector address
-# 4) vector coordenada (ll)
-# 5) vector (address, coordenada)
-
-# crear un csv "pequeño" (varios fallos)
-# 1) address
-# 2) coordenada (ll) 
-# 3) vector (address, coordenada)
-
-# geo.connect()
-
-expect_that(geo.connect("Madrid"), 
-            equals(data.frame(latitude = 40.41678, 
-                              longitude = -3.70379,
-                              address = "Madrid, Spain",
-                              stringsAsFactors = FALSE),
-                   tolerance = 0.00001))
-
-expect_equal(geo.connect("40.41678, -3.70379"), 
-             data.frame(latitude = 40.41671 , 
-                        longitude = -3.703779,
-                        address = "Plaza Puerta del Sol, 11, 28013 Madrid, Spain",
-                        stringsAsFactors = FALSE),
-             tolerance = 0.00001)
-            
-expect_equal(geo.connect("12.5asa"), 
-             data.frame(latitude=0, 
-                        longitude=0, 
-                        address="ERROR", 
-                        stringsAsFactors = FALSE))
+expect_true(is.data.frame(geo_connect("Madrid")))
+expect_true(is.data.frame(geo_connect_vector(c("Madrid", "Linkoping"))))
+expect_true(is.data.frame(geo_connect_csv("D:/Advanced Programming in R/Lab5/address2.txt")))
 
 
-# geo.connect.vector()
+# geo_connect()
 
-expect_equal(geo.connect.vector(c("Madrid", "Rydsvagen 254, Linkoping")), 
-             data.frame(latitude = c(40.41678, 58.41236), 
-                        longitude = c(-3.70379, 15.56107),
-                        address = c("Madrid, Spain", 
-                                    "Rydsvägen 254, 584 31 Linköping, Sweden"),
-                        stringsAsFactors = FALSE),
-             tolerance = 0.000001)
+test_that("testing function geo_connect",{
+  # address 
+  expect_equal(geo_connect("Madrid"), 
+          data.frame(latitude = 40.41678, 
+                            longitude = -3.70379,
+                            address = "Madrid, Spain",
+                            stringsAsFactors = FALSE),
+                     tolerance = 0.00001)
+  # coordenates
+  expect_equal(geo_connect("40.41678, -3.70379"), 
+           data.frame(latitude = 40.41671 , 
+                      longitude = -3.703779,
+                      address = "Plaza Puerta del Sol, 11, 28013 Madrid, Spain",
+                      stringsAsFactors = FALSE),
+           tolerance = 0.00001)
+  # address ERROR         
+  expect_equal(geo_connect("12.5asadsd"), 
+           data.frame(latitude=0, 
+                      longitude=0, 
+                      address="ERROR", 
+                      stringsAsFactors = FALSE))
+})
 
-expect_equal(geo.connect.vector(c("Madrid", "58.41236, 15.56107")), 
-             data.frame(latitude = c(40.41678, 58.41236), 
-                        longitude = c(-3.70379, 15.56107),
-                        address = c("Madrid, Spain", 
-                                    "Rydsvägen 254, 584 31 Linköping, Sweden"), 
-                        stringsAsFactors = FALSE),
-             tolerance = 0.000001)
+# geo_connect_vector()
 
-expect_equal(geo.connect.vector(c("40.41678, -3.70379", "58.41236, 15.56107")), 
-             data.frame(latitude = c(40.41678, 58.41236), 
-                        longitude = c(-3.70379, 15.56107),
-                        address = c("Plaza Puerta del Sol, 11, 28013 Madrid, Spain", 
-                                    "Rydsvägen 254, 584 31 Linköping, Sweden"), 
-                        stringsAsFactors = FALSE),
-             tolerance = 0.00001)
+test_that("testing function geo_connect_vector",{
+  # address vector
+  expect_equal(geo_connect_vector(c("Madrid", "Rydsvagen 254, Linkoping")), 
+               data.frame(latitude = c(40.41678, 58.41236), 
+                          longitude = c(-3.70379, 15.56107),
+                          address = c("Madrid, Spain", 
+                                      "Rydsvägen 254, 584 31 Linköping, Sweden"),
+                          stringsAsFactors = FALSE),
+               tolerance = 0.000001)
+  # vector of address plus coordenates
+  expect_equal(geo_connect_vector(c("Madrid", "58.41236, 15.56107")), 
+               data.frame(latitude = c(40.41678, 58.41236), 
+                          longitude = c(-3.70379, 15.56107),
+                          address = c("Madrid, Spain", 
+                                      "Rydsvägen 254, 584 31 Linköping, Sweden"), 
+                          stringsAsFactors = FALSE),
+               tolerance = 0.000001)
+  # coordenates vector
+  expect_equal(geo_connect_vector(c("40.41678, -3.70379", "58.41236, 15.56107")), 
+               data.frame(latitude = c(40.41678, 58.41236), 
+                          longitude = c(-3.70379, 15.56107),
+                          address = c("Plaza Puerta del Sol, 11, 28013 Madrid, Spain", 
+                                      "Rydsvägen 254, 584 31 Linköping, Sweden"), 
+                          stringsAsFactors = FALSE),
+               tolerance = 0.00001)
+})
 
-# geo.connect.csv()
+# geo_connect_csv()
 
-csv <- read.csv("D:/Advanced Programming in R/Lab5/address.txt", sep = ";")
-csv
-expect_equal(geo.connect.csv(csv), 
-             data.frame(latitude = c(48.85837, 38.89768),
-                        longitude = c(2.294481, -77.036530),
-                        address = c("Eiffel Tower, Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
-                                    "The White House, 1600 Pennsylvania Ave NW, Washington, DC 20500, USA"),
-                        stringsAsFactors = FALSE),
-             tolerance = 0.00001)
+test_that("testing function geo_connect_csv",{
 
+  expect_equal(geo_connect_csv("D:/Advanced Programming in R/Lab5/address2.txt"), 
+               data.frame(latitude = c(48.85837, 38.89768, 41.89068, 40.64131, 0.00000, 0.00000),
+                          longitude = c(2.294481, -77.036530, 12.49134, -73.77814, 0.00000, 0.00000),
+                          address = c("Eiffel Tower, Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+                                      "The White House, 1600 Pennsylvania Ave NW, Washington, DC 20500, USA",
+                                      "Piazza del Colosseo, 58, 00184 Roma, Italy",
+                                      "John F. Kennedy International Airport (JFK), Queens, NY 11430, USA",
+                                      "ERROR",
+                                      "ERROR"),
+                          stringsAsFactors = FALSE),
+               tolerance = 0.000001)
+})
 
 
 
