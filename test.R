b8f0dbf240d08b1047046054cc54ff099823e2c5
pepelu8000 <joselu_1993@hotmail.es>
2016-09-29 16:26:12 +0200

    testing 2.0

--- test.R
+++ test.R
@@ -1,26 +1,23 @@
 # testing 
 
 library(testthat)
-library(geoloc)
-
-URL <- URLencode(paste("http://maps.google.com/maps/api/geocode/json?address=", address, sep=""))
-URL.data <- fromJSON(getURL(URL))
-address <- "white house"
-URL.data
-
-# crear por separado vectores para address y coordenadas
-lat <- c(40.41678, 58.41081)
-lon <- c(-3.70379,  15.62137)
-add <- c("Madrid, Spain", "Linkoping, Sweden")
+#library(geoloc) ???!!!
 
 
-x <- data.frame(latitude = 40.41678, 
-                longitude = -3.70379,
-                address = "Madrid, Spain")
+# creating some examples for testing
+ad <- c("Madrid", "Rydsvagen 254, Linkoping")
+coo.mad <- c(40.41678, -3.70379)
+coo.lin <- c(58.41236, 15.56107 )
+add <- c("Madrid, Spain", "Rydsvägen 254, 584 31 Linköping, Sweden")
 
-y <- data.frame(latitude = 58.41081, 
-                longitude = 15.62137,
-                address = "Linköping, Sweden")
+# testing the limits of the API configuration  ???!!!
+# URL.data$status == "OK"
+
+# testing the inputs and outputs of our functions
+# como controlo address del geo.connect  ??!!
+expect_true(is.data.frame(class(geo.connect("Madrid"))))
+expect_true(is.data.frame(class(geo.connect.vector(c("Madrid", "Linkoping")))))
+expect_true(is.data.frame(class(geo.connect.csv(""))))
 
 # pruebas:
 # 1) address
@@ -34,63 +31,67 @@ y <- data.frame(latitude = 58.41081,
 # 2) coordenada (ll) 
 # 3) vector (address, coordenada)
 
-test_that("geo.connect", {
-  expect_equal(as.data.frame(geo.connect("Madrid")), 
-               data.frame(latitude = 40.41678, 
-                          longitude = -3.70379,
-                          address = "Madrid, Spain", 
-                          stringsAsFactors = FALSE))
-})
+# geo.connect()
+
+expect_that(geo.connect("Madrid"), 
+            equals(data.frame(latitude = 40.41678, 
+                              longitude = -3.70379,
+                              address = "Madrid, Spain",
+                              stringsAsFactors = FALSE),
+                   tolerance = 0.00001))
+
+expect_equal(geo.connect("40.41678, -3.70379"), 
+             data.frame(latitude = 40.41671 , 
+                        longitude = -3.703779,
+                        address = "Plaza Puerta del Sol, 11, 28013 Madrid, Spain",
+                        stringsAsFactors = FALSE),
+             tolerance = 0.00001)
+            
+expect_equal(geo.connect("12.5asa"), 
+             data.frame(latitude=0, 
+                        longitude=0, 
+                        address="ERROR", 
+                        stringsAsFactors = FALSE))
 
 
-test_that("geo.connect.vector", {
-  expect_equal(as.data.frame(geo.connect.vector(address)), 
-               data.frame(latitude = 40.41678, 
-                          longitude = -3.70379,
-                          address = "Madrid, Spain", 
-                          stringsAsFactors = FALSE))
-})
+# geo.connect.vector()
+
+expect_equal(geo.connect.vector(c("Madrid", "Rydsvagen 254, Linkoping")), 
+             data.frame(latitude = c(40.41678, 58.41236), 
+                        longitude = c(-3.70379, 15.56107),
+                        address = c("Madrid, Spain", 
+                                    "Rydsvägen 254, 584 31 Linköping, Sweden"),
+                        stringsAsFactors = FALSE),
+             tolerance = 0.000001)
+
+expect_equal(geo.connect.vector(c("Madrid", "58.41236, 15.56107")), 
+             data.frame(latitude = c(40.41678, 58.41236), 
+                        longitude = c(-3.70379, 15.56107),
+                        address = c("Madrid, Spain", 
+                                    "Rydsvägen 254, 584 31 Linköping, Sweden"), 
+                        stringsAsFactors = FALSE),
+             tolerance = 0.000001)
+
+expect_equal(geo.connect.vector(c("40.41678, -3.70379", "58.41236, 15.56107")), 
+             data.frame(latitude = c(40.41678, 58.41236), 
+                        longitude = c(-3.70379, 15.56107),
+                        address = c("Plaza Puerta del Sol, 11, 28013 Madrid, Spain", 
+                                    "Rydsvägen 254, 584 31 Linköping, Sweden"), 
+                        stringsAsFactors = FALSE),
+             tolerance = 0.00001)
+
+# geo.connect.csv()
+
+csv <- read.csv("D:/Advanced Programming in R/Lab5/address.txt", sep = ";")
+csv
+expect_equal(geo.connect.csv(csv), 
+             data.frame(latitude = c(48.85837, 38.89768),
+                        longitude = c(2.294481, -77.036530),
+                        address = c("Eiffel Tower, Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+                                    "The White House, 1600 Pennsylvania Ave NW, Washington, DC 20500, USA"),
+                        stringsAsFactors = FALSE),
+             tolerance = 0.00001)
 
 
 
 
-
-
-### LAB 4 ###
-
-
-# Input error for function QRlinreg
-test_that("Input error for function QRlinreg: ", {
-  expect_error(QRlinreg("a", iris), "class(formula) == \"formula\" is not TRUE", fixed = TRUE)
-  expect_error(QRlinreg(faithful$eruptions ~ faithful$waiting, "str"), "is.data.frame(data) is not TRUE", fixed = TRUE)
-})
-
-# testing expected output of our methods:
-# for each one we had to round the results 
-
-# coef() method
-test_that("coef() method", {
-  expect_equal(obj.iris$coefficients, lm.iris$coefficients)
-  expect_equal(obj.faithful$coefficients, lm.faithful$coefficients)
-})
-
-# resid() method
-test_that("Residuos correct values", {
-  expect_equal(obj.iris$residuals, lm.iris$residuals)
-  expect_equal(obj.faithful$residuals, lm.faithful$residuals)
-})
-
-# pred() method
-test_that("pred() method", {
-  expect_equal(obj.iris$predicted, lm.iris$fitted.values)
-  expect_equal(obj.faithful$predicted, lm.faithful$fitted.values)
-})
-
-
-#  testing linreg and attributes' class
-test_that("testing classes", {
-  expect_equal(class(obj.iris)[1],"linreg")
-  expect_true(is.numeric(obj.iris$coefficients))
-  expect_true(is.numeric(obj.iris$residuals))
-  expect_true(is.numeric(obj.iris$predicted))
-})
